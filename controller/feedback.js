688f4cf94282fd3ec7b7808578f65b486a1e6189
spex2024 <dercolbags@gmail.com>
2024-07-18 15:09:58 +0000

    controller updated .

--- controller/feedback.js
+++ controller/feedback.js
@@ -162,6 +162,7 @@ export const getFeedback = async (req, res) => {
 export const vendorFeedback = async (req, res) => {
     try {
         const postData = await Vendor.find().sort({ createdAt: -1 });
+        res.json(postData)
     } catch (error) {
         console.error('Error fetching posts:', error);
         res.status(500).json({ error: 'Internal server error' });
